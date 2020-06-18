522570a336ca2749cad52ffdd04ecbacbe53a5d6
ConorNicol <conor.nicol@mindbridge.ai>
2020-06-18 11:43:55 -0400

    whoops

--- src/objects/Land/Land.js
+++ src/objects/Land/Land.js
@@ -1,8 +1,5 @@
 import { Group, ObjectLoader } from 'three';
-// import { LegacyJSONLoader } from 'three/examples/jsm/loaders/deprecated/LegacyJSONLoader.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import MODEL from './land.json';
-import DOG from './dog/scene.gltf';
 
 export default class Land extends Group {
   constructor() {
